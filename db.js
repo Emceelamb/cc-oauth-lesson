8f21aa38c719e6ac81923cc0f773651b1adebda7
emceelamb <mlam@berkeley.edu>
2021-05-19 17:50:43 -0400

    Project starting code.

---
+++ db.js
@@ -0,0 +1,11 @@
+module.exports =
+{
+  confidentialClients: [{
+    clientId: '<CLIENT_ID>',
+    clientSecret: '<CLIENT_SECRET>',
+    grants: [
+      'client_credentials'
+    ],
+  }],
+  tokens:[]
+}
